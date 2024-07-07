9584f3244c817458dc12a674f9940b62e24ccd58
nab138 <56705177+nab138@users.noreply.github.com>
2024-07-06 22:37:13 -0400

    Remove broken sockjs fork

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,2 +1,6 @@
 declare module "sockjs-client/lib/transport/browser/abstract-xhr";
+declare module 'sockjs-client/dist/sockjs.min.js' {
+    import Client from 'sockjs-client';
+    export default Client;
+}
 declare module 'react-console-emulator'
